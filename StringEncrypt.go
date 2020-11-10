5d443f0908a561165192f604f0926f0f01673b5f
Pritindra <37242669+pritindra@users.noreply.github.com>
2020-11-10 10:00:10 +0530

    Create StringEncrypt.go

---
+++ StringEncrypt.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func encrpyt (string s) string {
+	l := len(text)
+	b := math.Ceil(math.Sqrt(l))
+	a := math.Floor(math.Sqrt(l))
+	encrpyted := ""
+	if b*a < l {
+		if math.Min(b,a) = b {
+			b := b + 1
+		} else {
+			a := a + 1
+		}
+	}
+
+	var arr []string
+	var i,j int
+	arr := [['' for i := range(a)] for j in range(b)]
+	var k := 0
+
+	for j := range a{
+		for i := range b {
+			if k<l {
+				arr[j][i] = s[k]
+			}
+			k := k + 1
+		}
+	}
+
+	for j := range b {
+		for i in range a{
+			encrpyted := encrpyted + arr[i][j]
+		}
+	}
+	return encrpyted
+}
