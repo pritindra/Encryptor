fcab20e35cafccf37ab6ab9dd8b1aedc58cd9b40
Pritindra <37242669+pritindra@users.noreply.github.com>
2020-11-11 08:47:57 +0530

    Update StringEncrypt.go

--- StringEncrypt.go
+++ StringEncrypt.go
@@ -39,3 +39,35 @@ func encrpyt (string s) string {
 	}
 	return encrpyted
 }
+
+
+func decrypt (string s) string {
+	l := len(text)
+	b := math.Ceil(math.Sqrt(l))
+	a := math.Floor(math.Sqrt(l))
+	decrypted := ""
+	
+
+	var arr []string
+	var i,j int
+	arr := [['' for i := range(a)] for j in range(b)]
+	var k := 0
+
+	for j := range a{
+		for i := range b {
+			if k<l {
+				arr[j][i] = s[k]
+			}
+			k := k + 1
+		}
+	}
+
+	for j := range b {
+		for i in range a{
+			decrypted := decrypted + arr[i][j]
+		}
+	}
+	return decrypted
+}
+
+
