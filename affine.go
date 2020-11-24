72cd36d4d97149b2ab72ff304b97e235b126c5a5
Pritindra <37242669+pritindra@users.noreply.github.com>
2020-11-24 09:32:39 +0530

    Update affine.go

--- affine.go
+++ affine.go
@@ -14,3 +14,26 @@ func affineEncrypt(string msg) string {
   }
   return cipher
 }
+
+func affineDecrpyt(string cipher) string {
+  msg := ""
+  a_inv := 0
+  flag := 0
+  var i int
+  for i:=0; i<26; i++ {
+    flag := (a*i) % 26
+    if flag = 1 {
+      a_inv = i
+    }
+  }
+  for i:=0; i < len(cipher); i++  {
+    if cipher[i] != ' '{
+      msg := msg + char(((a_inv*((cipher[i]+'A' - b))%26)) + 'A')
+    }else {
+      msg := msg + cipher[i]
+    }
+    
+    
+  }
+  return msg
+}
